ec6bda03c9a572495b1cf5e45eff9a8255213da8
Toadettek <oliviakitty1989@gmail.com>
2020-08-12 16:27:19 +0800

    commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var firstName = 'John';
+var age = 28;
+
+// Type coercion
+console.log(firstName + ' ' + age);
+
+// declare multiple variables 
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + 'years old'+ job +'. Is he married?'+ isMarried);
+
+
+//variable mutation
+age = 'twenty eight';
+job = 'driver';
+alert(firstName + ' is a ' + age + 'years old '+ job +'. Is he married?'+ isMarried);
+
+var lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName);
