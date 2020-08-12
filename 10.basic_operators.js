feef4b6d42610493c6f7b2ed064ed5dfa7b8b6ee
Toadettek <oliviakitty1989@gmail.com>
2020-08-12 16:49:05 +0800

    commit

---
+++ 10.basic_operators.js
@@ -0,0 +1,29 @@
+/* 
+**Basic operators
+*/
+
+var year, yearJohn, yearMark;
+now = 2020;
+ageJonh = 28;
+ageMark = 33;
+
+//Math operators
+var yearJohn = now - ageJonh;
+var yearMark = now - ageMark;
+
+console.log(yearJohn);
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 2);
+
+//Logical operators
+var johnOlder = ageJonh > ageMark;
+console.log(johnOlder);
+
+
+//typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJonh);
+console.log(typeof 'candela');
+var x;
+console.log(typeof x);
