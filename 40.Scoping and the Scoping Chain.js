702b2169ec2a18f86b23d6b97b730ded3de8fc30
bdabyjg <oliviakitty1989@gmail.com>
2020-09-06 19:09:40 +0800

    .

---
+++ 40.Scoping and the Scoping Chain.js
@@ -0,0 +1,33 @@
+
+
+// var a = "hello";
+// first();
+
+// function first(){
+//     var b = "hi";
+//     second();
+
+//     function second(){
+//         var c = "hey";
+//         console.log(a+b+c);
+//     }
+// }
+
+//Another example
+
+var a = "hello";
+first();
+
+function first(){
+    var b = "hi";
+    second();
+
+    function second(){
+        var c = "hey";
+        console.log(a+b+c);
+    }
+}
+function third(){
+    var d = 'John';
+    console.log(c);
+}
