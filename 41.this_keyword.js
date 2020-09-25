8cd9386f7d7369c7d26328232d039e509f7f5362
bdabyjg <oliviakitty1989@gmail.com>
2020-09-25 15:38:06 +0800

    ..

--- 41.this_keyword.js
+++ 41.this_keyword.js
@@ -15,6 +15,22 @@ var john = {
     yearOfBirth: 1990,
     calculateAge: function () {
         console.log(this);
+        console.log(2020 - this.yearOfBirth);
+
+
+        // function innerFunction(){
+        //     console.log(this);
+        // }
+        // innerFunction();
     }
 }
 john.calculateAge();
+
+var mark = {
+    name: 'Mark',
+    yearOfBirth: 1986,
+
+}
+//borrowing method
+mark.calculateAge = john.calculateAge;
+mark.calculateAge();
