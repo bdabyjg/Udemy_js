d49bfcf1ec16d5babf5d681306cfb9520619c073
Toadettek <oliviakitty1989@gmail.com>
2020-12-20 19:23:21 +0800

    ..

--- Section1(New version)/Scripts.js
+++ Section1(New version)/Scripts.js
@@ -1,1 +1,24 @@
-let js ="amazing";
+// let js ="amazing";
+//
+// let firstName = "Bob";
+// console.log(firstName);
+
+// _和$可以用于变量名之前，唯二的两个符号
+//
+// let myFirstJob = 'Programmer';
+// let myCurrentJob = 'Teacher';
+//
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+//
+// console.log(typeof true);
+
+
+// var is the old way to declare variables
+
+//use let to change type
+let age = 30;
+age = 31;
+
+//use const can not be changed or mutated
+const birthYear = 1991;
