316daecbf3e223fd8ad6af7d2bc9f6f78602c724
Toadettek <oliviakitty1989@gmail.com>
2020-12-22 20:36:55 +0800

    ..

--- Section1(New version)/Scripts.js
+++ Section1(New version)/Scripts.js
@@ -1,3 +1,5 @@
+// All of the Fundamentals
+
 // let js ="amazing";
 //
 // let firstName = "Bob";
@@ -17,8 +19,28 @@
 // var is the old way to declare variables
 
 //use let to change type
-let age = 30;
-age = 31;
+// let age = 30;
+// age = 31;
+//
+// //use const can not be changed or mutated
+// const birthYear = 1991;
 
-//use const can not be changed or mutated
-const birthYear = 1991;
+
+const now = 2037;
+const ageJonas = now- 1991;
+const ageSarah = now-2018;
+console.log(ageJonas,ageSarah);
+
+console.log(ageJonas * 2, ageJonas/10, 2**3)
+// 2**3 means 2 to the power of 3 = 2^3= 2*2*2
+
+const firstName = 'Candela';
+const lastName = 'Kitty';
+console.log(firstName + ''+ lastName);
+
+
+let x = 10+5;
+x += 10; //x = x+10
+x *= 4;
+x++; //x = x+1
+console.log(x)
