c5eb90726dea8902e728c66e2cc615612f85913a
Toadettek <oliviakitty1989@gmail.com>
2021-01-25 01:58:58 +0800

    Template literal

---
+++ Section1(New version)/Challenge1.js
@@ -0,0 +1,11 @@
+
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.86;
+
+const BMIMark = massMark/heightMark **2;
+const BMIJohn = massJohn/heightJohn **2;
+
+console.log(BMIMark >BMIJohn);
---
+++ Section1(New version)/Part1 Template literal.js
@@ -0,0 +1,10 @@
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthyear = 1989;
+const year = 2038;
+const jonas = "I'm "+ firstName + '.';
+
+const jonasNew = `I'm ${firstName}, a ${year-birthyear} year old ${job}!`;
+console.log(jonasNew)
+
