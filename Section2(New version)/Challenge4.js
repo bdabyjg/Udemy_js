f70cf967a3c63b25ae07f9227a1f83c2d3a7556e
toadette <toadettek@outlook.com>
2021-03-09 00:01:03 +0800

    challenge4 right answer

--- Section2(New version)/Challenge4.js
+++ Section2(New version)/Challenge4.js
@@ -5,12 +5,14 @@ let percentage;
 let total = [];
 
 
-function calculateTips(){
-    for (i in bills,i = 0, i <bills.length,i++){
-        bills>=300 ? percentage = 0.2 : percentage = 0.15;
-        tips = bills* percentage;
-        total = bills + tips;
-        console.log(tips,total)
-    }
+function calculateTips(bills,tips,percentage,total){
+    for (i = 0;i <bills.length;i++){
+        bills[i]>=300 ? percentage = 0.2 : percentage = 0.15;
+        tips[i] = bills[i]* percentage;
+        total[i] = bills[i] + tips[i];
+
+    };
+    console.log(tips,total)
 };
-console.log(`tips is ${tips} and total is ${total}`)
+calculateTips(bills,tips,percentage,total);
+// console.log(`tips is ${tips} and total is ${total}`)
