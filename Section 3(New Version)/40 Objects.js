7aa253aa21f57e3ae8eade5753ce8eafe87b7fbd
toadette <toadettek@outlook.com>
2021-03-18 17:54:26 +0800

    ..

--- Section 3(New Version)/40 Objects.js
+++ Section 3(New Version)/40 Objects.js
@@ -1,11 +1,12 @@
 
 // Creating Objects
-const Candela = {
-    firstName : 'Candela',
-    lastName :'Koala',
-    type: 'Maine Coon',
-    favorite : 'Meats'
-}
+// const Candela = {
+//     firstName : 'Candela',
+//     lastName :'Koala',
+//     type: 'Maine Coon',
+//     favorite : 'Meats',
+//     friends:['Palico','Tigrex','Ratha']
+// }
 // console.log(Candela.favorite);
 // console.log(Candela['type']);
 //
@@ -13,5 +14,29 @@ const Candela = {
 // console.log(Candela['first' + nameKey]);
 // console.log(Candela['last' + nameKey]);
 
-const interestedIn = prompt('What do you want to know about Candela? Choose between firstName,lastName,type,favorite');
-console.log(interestedIn);
+// const interestedIn = prompt('What do you want to know about Candela? Choose between firstName,lastName,type,favorite');
+// console.log(Candela[interestedIn]);
+// if(Candela[interestedIn]){
+//     console.log(Candela[interestedIn])
+// }else{
+//     console.log('Wrong request!')
+// }
+//
+// Candela.location = 'New York';
+// console.log(`${Candela.firstName} has  ${Candela.friends.length} friends, and her best friend is called ${Candela.friends[0]} `);
+
+// Objects Operation
+const Kitty = {
+    firstName:'Toadette',
+    birthYear:1989,
+    job:'Developer',
+    friends:['Koala','Maine Coon','Airou','Palico','Ratha'],
+    hasDriveLicense:false,
+    calcAge:function(){
+        console.log(this);
+        return 2037 - this.birthYear
+    }
+};
+
+console.log(Kitty.calcAge());
+
--- Section 3(New Version)/Code Challenge 2.js
+++ Section 3(New Version)/Code Challenge 2.js
@@ -1,17 +1,16 @@
 
 const bills = [125, 555, 44];
 const tips = [];
-let percentage;
 
 
-function calcTip(bills,tips,percentage){
+
+function calcTip(bills,tips){
     for (i=0;i<bills.length;i++){
-        bills[i] >= 300 ? percentage = 0.2 : percentage = 0.15;
-        tips[i] = bills[i] * percentage;
+        bills[i] <= 300&& bills[i] > 50 ?  tips[i] = bills[i] * 0.15 : tips[i]=bills[i]*0.2
         console.log(tips)}
 
 }
-console.log(calcTip(bills,tips,percentage))
+console.log(calcTip(bills,tips))
 
 //老师的写法 箭头函数
-const calcTip = bill => bill>=50 && bill<=300 ? bill * 0.15 : bill * 0.2;
+// const calcTip = bill => bill>=50 && bill<=300 ? bill * 0.15 : bill * 0.2;
