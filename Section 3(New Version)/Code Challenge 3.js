4807591f00ea94e25853ac05c7ae305a237eab81
toadette <toadettek@outlook.com>
2021-04-12 18:00:03 +0800

    ..

---
+++ Section 3(New Version)/Code Challenge 3.js
@@ -0,0 +1,22 @@
+
+const Mark = {
+    firstName:'Mark',
+    weight: 78,
+    height: 1.69,
+    BMIcalc:function (){
+        Mark.BMI = Mark.weight/(Mark.height)^2
+    }
+};
+
+const John = {
+    firstName:'John',
+    weight: 92,
+    height: 1.95,
+    BMIcalc:function (){
+        John.BMI = John.weight/(John.height)^2
+    }
+}
+
+function summary(Mark,John){
+
+}
