9a52bd49f7f6ab7faf3b07e3137dbc202833dc41
toadette <toadettek@outlook.com>
2021-04-30 12:10:37 +0800

    2021430上午

--- Section 5/script.js
+++ Section 5/script.js
@@ -6,7 +6,21 @@ document.querySelector('message').textContent = 'Correct Number';  //修改相�
 
 console.log(document.querySelector('.guess').value);// 获取输入框中的值 */
 
+const secretNumber = Math.trunc(Math.random()*20+1);
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click',function (){
-    console.log(document.querySelector('.guess').value)
+    const guess = Number(document.querySelector('.guess').value);
+
+    if(!guess){
+        document.querySelector('.message').textContent = 'No number!';
+    }else if(guess === secretNumber){
+        document.querySelector('.message').textContent = 'Correct Number!';
+    }else if(guess > secretNumber){
+        document.querySelector('.message').textContent = 'Too high!';
+    }else if(guess < secretNumber){
+        document.querySelector('.message').textContent = 'Too low!';
+    }
+
+
 })
