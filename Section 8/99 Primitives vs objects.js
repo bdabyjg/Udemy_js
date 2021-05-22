2e01840dc3cbd885d76882358ab61d7b61f09f8c
toadette <toadettek@outlook.com>
2021-05-22 15:14:58 +0800

    2021.5.22 End section 8

--- Section 8/99 Primitives vs objects.js
+++ Section 8/99 Primitives vs objects.js
@@ -1,18 +1,49 @@
+//
+//
+// let age = 30;
+// let oldAge = age;
+// age = 31;
+//
+// console.log(age);
+// console.log(oldAge);
+//
+// const me = {
+//     name:'Kitty',
+//     age:30,
+// }
+//
+// const friend = me;
+// friend.age = 27;
+// console.log('Friend:',friend);
+// console.log('Me:',me);
 
 
-let age = 30;
-let oldAge = age;
-age = 31;
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
 
-console.log(age);
-console.log(oldAge);
+console.log(lastName,oldLastName);
 
-const me = {
-    name:'Kitty',
-    age:30,
+// const jessica = {
+//     firstName:'Jessica',
+//     lastName:'Williams',
+//     age:27
+// }
+
+// const marriedJessica = jessica;
+// marriedJessica.lastName = 'Davis';
+// console.log('Before marriage:',jessica);
+// console.log('After marriage:',marriedJessica);
+const jessica2 = {
+    firstName:'Jessica',
+    lastName:'Williams',
+    age:27,
+    family:['Alice','Bob']
 }
+const jessicaCopy = Object.assign({},jessica2);
+jessicaCopy.lastName = 'Davis';
+console.log('Before marriage:',jessica2);
+console.log('After marriage:',jessicaCopy);
 
-const friend = me;
-friend.age = 27;
-console.log('Friend:',friend);
-console.log('Me:',me);
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
