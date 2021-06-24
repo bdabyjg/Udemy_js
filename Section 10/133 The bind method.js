51746657bb38c60d16e11c605c5284e6904b793d
toadette <toadettek@outlook.com>
2021-06-24 18:12:53 +0800

    2021.6.24下午 Start Section 11

--- Section 10/133 The bind method.js
+++ Section 10/133 The bind method.js
@@ -29,22 +29,25 @@ const bookEW =book.bind(eurowings);
 const bookLH = book.bind(lufthansa);
 const bookLX = book.bind(swiss);
 
+//也可以先预设第一个argument，也就是23， euro依然是指派的this的对象
 const bookEW23 = book.bind(eurowings,23);
+//这里的ratha就变成了最早book方法里面的第二个argument
 // bookEW23('Ratha');
 
-
+// With Event Listeners
 lufthansa.planes = 300;
 lufthansa.buyPlane = function (){
     console.log(this);
     this.planes++;
     console.log(this.planes);
 }
+// lufthansa.buyPlane();
 document.querySelector('.buy').addEventListener('click',lufthansa.buyPlane.bind(lufthansa));
 
 const addTax = (rate,value)=>value+value*rate;
 
 //addVAT = value =>value+value*0.23;
-const addVAT = addTax.bind(null,0.23);
+const addVAT = addTax.bind(0.23);
 console.log(addVAT(200));
 
 const addTaxRate = function (rate){
@@ -53,4 +56,4 @@ const addTaxRate = function (rate){
     }
 }
 const addVAT2 = addTaxRate(0.23);
-console.log(addVAT2(200));
+// console.log(addVAT2(200));
