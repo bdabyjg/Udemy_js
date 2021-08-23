77f0ba47a9446e9b9220c42048d053f4dfc058ee
bdabyjg <oliviakitty@gmail.com>
2021-08-23 16:26:46 +0800

    2021.8.23 Start Section 15

---
+++ Section 15/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+
+//调用地理位置api:navigator.geolocation
+if (navigator.geolocation)
+navigator.geolocation.getCurrentPosition(function (position){
+    const {latitude} = position.coords;
+    const {longitude} = position.coords;
+    console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+    const coords = [latitude,longitude];
+
+    //引入Leaflet
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker(coords).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+},function (){
+    alert('Could not get your position.')
+})
