c475c73047722cf8b9300d3405a987b178c6ab41
bdabyjg <oliviakitty@gmail.com>
2021-08-24 17:34:23 +0800

    2021.8.24

--- Section 15/script.js
+++ Section 15/script.js
@@ -12,24 +12,68 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 
+class App{
+    #map;
+    #mapEvent;
+    constructor() {
+        this._getPosition();
+        form.addEventListener('submit',this._newWorkout.bind(this));
+        inputType.addEventListener('change',this._toggleElevationField);
+    }
+    _getPosition(){if (navigator.geolocation)
+        navigator.geolocation.getCurrentPosition(this._loadMap.bind(this),function (){
+            alert('Could not get your position.')
+        })
+
+    }
+    _loadMap(position){
+        const {latitude} = position.coords;
+        const {longitude} = position.coords;
+        // console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+        const coords = [latitude,longitude];
+
+        //引入Leaflet
+        this.#map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.#map);
+        //Handling clicks on map
+        this.#map.on('click',this._showForm.bind(this));
+
+    }
+    _showForm(mapE){
+        this.#mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+    }
+    _toggleElevationField(){
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+
+    }
+    _newWorkout(e){
+        e.preventDefault();
+
+        //Clear input fields;
+        inputDistance.value = inputDuration.value = inputCadence.value ='';
+
+        //Display marker
+        const { lat,lng }=this.#mapEvent.latlng;
+        L.marker([lat,lng]).addTo(this.#map).bindPopup(L.popup({
+            maxWidth:250,
+            minWidth:100,
+            autoClose:false,
+            closeOnClick:false,
+            className:'running-popup'
+        })).setPopupContent('Workout')
+            .openPopup();
+    }
+}
+
+const app = new App();
+
+
 //调用地理位置api:navigator.geolocation
-if (navigator.geolocation)
-navigator.geolocation.getCurrentPosition(function (position){
-    const {latitude} = position.coords;
-    const {longitude} = position.coords;
-    console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
-    const coords = [latitude,longitude];
 
-    //引入Leaflet
-    const map = L.map('map').setView(coords, 13);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    L.marker(coords).addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
-},function (){
-    alert('Could not get your position.')
-})
