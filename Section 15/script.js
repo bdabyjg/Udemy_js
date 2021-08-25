dcc3e47d892518f71da6ed57917606b50dd75d5c
bdabyjg <oliviakitty@gmail.com>
2021-08-25 17:47:44 +0800

    2021.8.25

--- Section 15/script.js
+++ Section 15/script.js
@@ -11,7 +11,47 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout{
+    date = new Date();
+    id = (Date.now()+'').slice(-10);
 
+    constructor(coords,distance,duration){
+      this.coords = coords;//[lat,lng]
+      this.distance = distance;
+      this.duration = duration;
+    }
+}
+
+class Running extends Workout{
+    constructor(coords,distance,duration,cadence){
+        super(coords,distance,duration);
+        this.cadence = cadence;
+        this.calcPace();
+
+
+    }
+    calcPace(){
+        this.pace = this.duration / this.distance;
+    }
+}
+class Cycling extends Workout{
+    constructor(coords,distance,duration,elevationGain){
+        super(coords,distance,duration);
+        this.elevationGain = elevationGain;
+        this.calcSpeed();
+    }
+    calcSpeed(){
+        this.speed = this.distance / (this.duration/60);
+        return this.speed;
+    }
+}
+
+const run1 = new Running([20,-110],5.2,24,178);
+const cyc1 = new Cycling([20,-110],27,95,523);
+console.log(run1,cyc1);
+
+
+//APPLICATION ARCHITECTURE
 class App{
     #map;
     #mapEvent;
@@ -54,11 +94,23 @@ class App{
     }
     _newWorkout(e){
         e.preventDefault();
+        //Steps:
+        // 1 Get data from form
+        const type = inputType.value;
+        const distance = +inputDistance.value;
+        const duration = +inputDuration.value;
+        // 2 Check if data is valid
+        // 3 If workout running, create running object
+        if (type === 'running'){
+            const cadence = +inputCadence.value;
+            if (!Number.isFinite(distance)) return alert('Inputs have to positive numbers!')
 
-        //Clear input fields;
-        inputDistance.value = inputDuration.value = inputCadence.value ='';
-
-        //Display marker
+        // 4 If workout cycling, create cycling object
+        if (type === 'cycling'){
+            const cadence = +inputElevation.value;
+        }
+        // 5 Add new object to workout array
+        // 6 Render workout on map as marker
         const { lat,lng }=this.#mapEvent.latlng;
         L.marker([lat,lng]).addTo(this.#map).bindPopup(L.popup({
             maxWidth:250,
@@ -68,10 +120,16 @@ class App{
             className:'running-popup'
         })).setPopupContent('Workout')
             .openPopup();
+        // 7 Render workout on list
+        //8 Hide form + Clear input fields;
+        inputDistance.value = inputDuration.value = inputCadence.value ='';
+
+
     }
 }
+}
 
-const app = new App();
+// const app = new App();
 
 
 //调用地理位置api:navigator.geolocation
