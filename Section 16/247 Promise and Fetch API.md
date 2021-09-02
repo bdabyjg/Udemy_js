c85cb8ac7a40a0f52d6d706c44812ff325b3d91b
bdabyjg <oliviakitty@gmail.com>
2021-09-02 15:47:24 +0800

    2021.9.2 250 Handling rejected promises

--- Section 16/247 Promise and Fetch API.md
+++ Section 16/247 Promise and Fetch API.md
@@ -9,3 +9,20 @@
 * Instead of nesting callbacks, we can chain promises for a sequence of asynchronous operations: escaping callback hell.
 
 ![](D:\Coding\Udemy_js\Section 16\247 Promises and the fetch API.png)
+
+
+
+Call fetch function will then immediately return a promise. As soon as we start a request, promise is still pending because the asynchronous task of getting the data is still running in the background. At a certain point the promise will then be settled and either in a fulfilled or in a rejected state.
+
+
+
+```javascript
+fetch(`https://restcountries.eu/rest/v2/name/${country}`).then(function (response){
+    console.log(response);
+    return response.json();
+}).then(function (data){
+    console.log(data);
+});
+```
+
+This fetch function here returning a promise. Then we handled that promise use the then method. Then the then method read the data from the response, we need to call the json method on that response object. Now this itself will also return a promise. If we then return that promise from this (then response) method, then basically all of this becomes a new promise itself. So since this is a promise, we can call the then method on that again. Then again we have a callback. This time we get access to the data because the resolved value of this promise here is going to be the data itself. The data we're looking for which is this one here(console: altSpelling).
--- Section 16/script.js
+++ Section 16/script.js
@@ -31,21 +31,21 @@ const countriesContainer = document.querySelector('.countries');
 // getCountryData('usa');
 // getCountryData('japan')
 
-// const renderCountry = function (data,className = '') {
-//     const html = `
-//           <article class="country ${className}">
-//           <img class="country__img" src="${data.flag}" />
-//           <div class="country__data">
-//             <h3 class="country__name">${data.name}</h3>
-//             <h4 class="country__region">${data.region}</h4>
-//             <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
-//             <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-//             <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-//           </div>
-//         </article>`;
-//     countriesContainer.insertAdjacentHTML('beforeend', html);
-//     countriesContainer.style.opacity = 1;
-// }
+const renderCountry = function (data, className = '') {
+    const html = `
+          <article class="country ${className}">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+          </div>
+        </article>`;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+}
 //
 // const getCountryAndNeighbor = function (country) {
 //
@@ -79,5 +79,25 @@ const countriesContainer = document.querySelector('.countries');
 // const request = new XMLHttpRequest();
 // request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
 // request.send();
-const request = fetch('https://restcountries.eu/rest/v2/name/usa');
-console.log(request);
+// const request = fetch('https://restcountries.eu/rest/v2/name/usa');
+// console.log(request);
+
+const getCountryData = function (country) {
+    //country 1
+    fetch(`https://restcountries.eu/rest/v2/name/${country}`).then(response => response.json()).then(data => {
+        renderCountry(data[0])
+        const neighbour = data[0].borders[0];
+        if (!neighbour) return;
+        // Country 2
+        return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    }).then(response=>response.json()).then(data=>renderCountry(data,'neighbour'));
+    //     (response){
+    //     return response.json();
+    // // }).then(function (data){
+    //     console.log(data);
+    //     renderCountry(data[0]);
+    // });
+};
+btn.addEventListener('click',function (){
+    getCountryData('germany');
+})
