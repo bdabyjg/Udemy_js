4cde9072c616de9ec135e7757b5bb11fcc489b43
bdabyjg <oliviakitty@gmail.com>
2021-09-06 16:08:43 +0800

    2021.9.4 Challenge 1 done

--- Section 16/Challenge 1.js
+++ Section 16/Challenge 1.js
@@ -1,10 +1,38 @@
 'use strict'
 
+const renderCountry = function (data, className = '') {
+    const html = `
+          <article class="country ${className}">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+          </div>
+        </article>`;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    // countriesContainer.style.opacity = 1;
+};
+
+
 const whereAmI = function (lat,lng){
    fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-       .then(res=>res.json())
+       .then(res=>{
+           if (!res.ok) throw new Error(`Problem with geocodidng ${res.status}`);
+           return res.json();
+       })
        .then(data=>{
        console.log(data);
+       return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`)
    })
+       .then(res=>{
+           if (!res.ok)
+               throw new Error(`Country not found (${res.status}`);
+           return res.json();
+       })
+       .then(data=>renderCountry(data,'neighbour'))
+       .catch(err=>console.error(`${err.message}`))
 }
-whereAmI(110.3,20.0);
+whereAmI(52.508,13.381);
